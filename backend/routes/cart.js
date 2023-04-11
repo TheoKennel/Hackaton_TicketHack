f8abc585132a8e68cc49c39d585202b50abac322
Baptiste <baptiste.kara@gmail.com>
2023-04-11 12:00:49 +0200

    ajout des routes pour le panier

--- backend/routes/cart.js
+++ backend/routes/cart.js
@@ -1,11 +1,23 @@
 var express = require('express');
 var router = express.Router();
 const Cart = require('../models/cart');
+const Trip = require('../models/trips');
 
 
 // ajout dans le panier
-router.post('/', (req,res)=>{
-
+router.post('/:id', (req,res)=>{
+    const { _id } = req.params;
+    Trip.findById(_id)
+    .then(data =>{
+        const newCart = new Cart({
+            departure: data.departure,
+            arrival: data.arrival,
+            date: data.date,
+            price: data.price
+        })
+        .save()
+        .then(res.json({result : true, cart: newCart}))
+    })
 })
 
 //affichage panier
@@ -21,7 +33,16 @@ router.get('/', (req, res)=>{
 
 //supprimer un element du panier
 router.delete('/:id', (req,res)=>{
-
+    Cart.deleteOne(id)
+    .then(deleteDoc =>{
+        if (deleteDoc.deletedCount > 0) {
+            Cart.find().then(data=>{
+                res.json({result: true, cart: data})
+            })
+        }else{
+            res.json({result: false, error : "problème suppression"})
+        }
+    })
 })
 
 
