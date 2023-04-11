776c8e664b28119f3d35a01c93590cda39a5ca7e
Théo <theokennel@gmail.com>
2023-04-11 14:24:17 +0200

    Rajout commentaire pour date formatage, pas fini

--- backend/routes/trips.js
+++ backend/routes/trips.js
@@ -1,14 +1,16 @@
 var express = require('express');
 var router = express.Router();
 require('../models/connection')
-const Trips = require('../models/trips')
+const Trips = require('../models/trips');
+const moment = require('moment/moment');
 
 router.get('/', (req,res) => {
     const { departure, arrival, date } = req.body;
+    // const dateUtc = moment.utc(date).format('YYYY-MM-DDTHH:mm:ss.SSSZ');
     Trips.find( {
         departure : { $regex: new RegExp(req.body.departure, "i") },
         arrival : { $regex: new RegExp(req.body.arrival, "i") },
-        date  // Date à vérifier, (format à modifier soit en back soit en front)
+        date : date // Date à vérifier, (format à modifier soit en back soit en front)
     }).then(data => {
         if(data.length > 0 ) {
             res.json({ result : true, trajet : data})
