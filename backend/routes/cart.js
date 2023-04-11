604e3b544ecdb0088549e0820a47c50a45c4df27
Baptiste <baptiste.kara@gmail.com>
2023-04-11 11:39:09 +0200

    add cart

--- backend/routes/cart.js
+++ backend/routes/cart.js
@@ -1,4 +1,27 @@
 var express = require('express');
 var router = express.Router();
+const Cart = require('../models/cart');
+
+
+// ajout dans le panier
+router.post('/', (req,res)=>{
+
+})
+
+//affichage panier
+router.get('/', (req, res)=>{
+    Cart.find().then(data=>{
+        if (!data) {
+            res.json({result : false})
+        }else{
+            res.json({result: true, trips : data})
+        }
+    })
+})
+
+//supprimer un element du panier
+router.delete('/:id', (req,res)=>{
+
+})
 
 module.exports = router;
