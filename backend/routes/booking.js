f00b5161abcc948045bc8da88f46710562da0391
Théo <theokennel@gmail.com>
2023-04-12 15:05:41 +0200

    Début front booking.js

--- backend/routes/booking.js
+++ backend/routes/booking.js
@@ -1,32 +1,47 @@
 var express = require('express');
 var router = express.Router();
 const booking = require('../models/booking')
-const Cart = require('../models/cart')
+const Cart = require('../models/cart');
 
 // Ajout dans le booking
-router.post('/:id', (req,res)=> {
-    const id = req.params.id
-    Cart.findById(id)
+router.post('/', (req,res)=> {
+    Cart.find()
     .then(data =>{
-        console.log(data);
-        const newBooking = new booking({
-            departure: data.departure,
-            arrival: data.arrival,
-            date: data.date,
-            price: data.price
-        })
-        newBooking.save()
-        .then(data=>{
-            res.json({result: true, booking: data})
+        if(!data.length > 1) {
+            console.log(data);
+            const newBooking = new booking({
+                departure: data.departure,
+                arrival: data.arrival,
+                date: data.date,
+                price: data.price
+            })
+            newBooking.save()
+            .then(data=>{
+                res.json({result: true, booking: data})
+            })
+        } else {
+            for(let i=0; i < data.length; i++) {
+                const newBooking = new booking({
+                    departure: data[i].departure,
+                    arrival: data[i].arrival,
+                    date: data[i].date,
+                    price: data[i].price
+                })
+                newBooking.save()
+                .then(data=>{
+                    let dataTabl = []
+                    dataTabl.push(data)})
+            }               
+        }
         })
     })
-})
+
 
 
 // Affichage dans booking
 router.get('/', (req,res) => {
     booking.find().then(data => {
-    if(!data) {
+    if(!data[0]) {
         res.json({result : false})
     } else {
         res.json({result: true, bookings : data})
