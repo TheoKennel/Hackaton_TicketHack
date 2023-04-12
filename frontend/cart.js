03de88f86da21c275168d5307e2e9a90aa049b2e
Baptiste <baptiste.kara@gmail.com>
2023-04-12 11:36:14 +0200

    affichage du panier

--- frontend/cart.js
+++ frontend/cart.js
@@ -1,10 +1,13 @@
 fetch("http://localhost:3000/cart")
   .then((response) => response.json())
   .then((data) => {
+    console.log(data.carts)
     if (data.carts) {
       let total = 0;
-      for (let i = 0; i < data.length; i++) {
-        let containerBody = document.querySelector(".container__bookings-trip");
+      const containerBody = document.querySelector(".container__bookings-trips-content");
+      for (let i = 0; i < data.carts.length; i++) {
+        const bookingsTrips= document.createElement("div");
+        bookingsTrips.classList.add("container__bookings-trips")
         const hour = String(new Date(data.carts[i].date).getHours()).padStart(
           2,
           "0"
@@ -12,13 +15,14 @@ fetch("http://localhost:3000/cart")
         const minute = String(
           new Date(data.carts[i].date).getMinutes()
         ).padStart(2, "0");
-        containerBody.innerHTML += `
+        bookingsTrips.innerHTML += `
                       <p>${data.carts[i].departure} > ${data.carts[i].arrival}</p>
                       <p>${hour}:${minute}</p>
                       <p>${data.carts[i].price}€</p>
                       <button class="root-search_content-button" value= ${data.carts[i]._id}>Book</button>
                   `;
-                  
+                  containerBody.appendChild(bookingsTrips);
+
                   total+=data.carts[i].price;
       }
       const containerBooking = document.querySelector(".container__bookings")
@@ -27,5 +31,7 @@ fetch("http://localhost:3000/cart")
       containerBooking.innerHTML = `<p>Total : 127€</p>
       <button>Purchase</button>`;
       containerBooking.appendChild(footerBooking);
+    }else{
+
     }
   });
